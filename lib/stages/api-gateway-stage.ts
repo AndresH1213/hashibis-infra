1579728a171df1952e6effdf70f2e28c303b9a37
Andres Hernandez <edanhebla1213@hotmail.com>
2023-09-04 20:48:01 -0500

    changing the id of apigateway stack in the stage

--- lib/stages/api-gateway-stage.ts
+++ lib/stages/api-gateway-stage.ts
@@ -9,13 +9,13 @@ export class ApiGatewayStage extends cdk.Stage {
   constructor(scope: Construct, id: string, props?: BasicStackProps) {
     super(scope, id, props);
 
-    new ApiGatewayStack(this, `api-gateway-${props?.stage}`, {
+    new ApiGatewayStack(this, `apigateway-${props?.stage}`, {
       env: {
         region: props?.env?.region,
         account: props?.env?.account,
       },
       stage: props?.stage || 'dev',
-      name: Util.getStackNameWithPrefix(`api-gateway-${props?.stage}`),
+      name: Util.getStackNameWithPrefix(`apigateway-${props?.stage}`),
     });
   }
 }
